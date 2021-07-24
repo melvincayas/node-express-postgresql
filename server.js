d15ceb04eb6321ba6899ebb9aac0f5b00ae93269
Melvin Cayas <cayasmj@gmail.com>
2021-07-24 10:31:16 -0400

    Created SignUp and Home routes

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const db = require("./db");
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+
+app.get("/signup", (req, res) => {
+	res.render("signup");
+});
+
+app.post("/signup", (req, res) => {});
+
+app.listen(5000, () => {
+	console.log("Now listening on Port 5000!");
+});
