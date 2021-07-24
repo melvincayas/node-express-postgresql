696f374780d913156983b0b4caa3fa7e27cd75f7
Melvin Cayas <cayasmj@gmail.com>
2021-07-24 12:49:29 -0400

    Added authentication with Passport.js

--- server.js
+++ server.js
@@ -2,10 +2,63 @@ const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const db = require("./db");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
+app.use(
+	session({ secret: "codingnewbie", resave: false, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+	new LocalStrategy(
+		{ usernameField: "email" },
+		async (email, password, done) => {
+			try {
+				const result = await db.query("SELECT * FROM users WHERE email = $1", [
+					email,
+				]);
+				if (result.rows.length === 0) return done(null, false);
+
+				const [foundUser] = result.rows;
+
+				bcrypt.compare(password, foundUser.password, (err, result) => {
+					if (err) return done(null, false);
+					return done(null, foundUser);
+				});
+			} catch (err) {
+				return done(null, false);
+			}
+		}
+	)
+);
+passport.serializeUser((user, done) => {
+	done(null, user.user_id);
+});
+passport.deserializeUser(async (id, done) => {
+	try {
+		const result = await db.query("SELECT * FROM users WHERE user_id = $1", [
+			id,
+		]);
+
+		if (result.rows.length === 0) return done(null, false);
+
+		const [foundUser] = result.rows;
+
+		return done(null, foundUser);
+	} catch (err) {
+		done(null);
+	}
+});
 
 app.get("/", (req, res) => {
 	res.render("index");
@@ -38,7 +91,14 @@ app.get("/login", (req, res) => {
 	res.render("login");
 });
 
+app.post("/login", passport.authenticate("local"), (req, res) => {
+	console.log("you made it here");
+
+	res.redirect("/");
+});
+
 app.use((err, req, res, next) => {
+	console.log(err);
 	res.render("error");
 });
 
